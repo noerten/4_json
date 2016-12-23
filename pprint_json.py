c73ffa53f109cce29a5b5f17899b88490debd43e
noerten <darkstria@gmail.com>
2016-12-24 01:23:00 +0300

    var names more specific

--- pprint_json.py
+++ pprint_json.py
@@ -3,19 +3,19 @@ import pprint
 import os
 
 
-def load_data(filepath):
+def load_json_file(filepath):
     if not os.path.exists(filepath):
         return None
     with open(filepath) as data_file:
-        data = json.load(data_file)
-    return data
+        json_object = json.load(data_file)
+    return json_object
 
 
-def pretty_print_json(data):
-    pprint.pprint(data)
+def pretty_print_json(json_object):
+    pprint.pprint(json_object)
 
 
 if __name__ == '__main__':
     file_name = input('Enter file name/path: ')
-    data = load_data(file_name)
-    pretty_print_json(data)
+    json_object = load_json_file(file_name)
+    pretty_print_json(json_object)
